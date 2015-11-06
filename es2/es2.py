590e5e995771ac0f4b9d4d6aa6434874248dbaa7
Luca Benci <zero@develer.com>
2015-11-06 10:55:37 +0100

    Refactoring di codice e test

--- es2/es2.py
+++ es2/es2.py
@@ -5,92 +5,80 @@
 # log, processi i file per estrarre da essi gli errori e li mostri a video
 # ordinati a partire dal più recente.
 
-# È opportuno indicare i file dal quale gli errori provengono?
-# Nota: Il programma prima legge tutte le linee di tutti i file nella directory
-# specificata, poi filtra solo le linee di errore. In caso di molti file di log
-# questo è inefficiente, sarebbe meglio leggere ogni file riga per riga e, se
-# la linea di testo è un errore solo allora aggiungerlo al testo salvato in
-# memoria
-
 import argparse
 import datetime
 import os
+import re
+
+def list_files(path, ext_list=["txt"]):
+    """
+    Esegue un walk nel path cercando tutti i file con una delle estensioni
+    definite dal parametro ext_list
+
+    Ritorna la lista dei file da leggere
+    """
+    log_files = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            # Lo splice [1:] qui sotto serve a escludere il dot dall'estensione
+            if (os.path.splitext(name)[1][1:] in ext_list):
+                log_files.append(os.path.join(root, name))
+    return log_files
+
+def filter_file(f, pattern="^(\d{4}\-\d{2}\-\d{2}\s{1}\d{2}\:\d{2}\:\d{2})\,(\d{1})\s{1}(Error\:.*)"):
+    """
+    Filtra il contenuto del file in modo tale da isolare gli errori
+
+    Ritorna una lista di tuple di tipo (datetime, int, str)
+    """
+    logs = []
+    with open(f, "r") as fd:
+        for line in fd:
+            try:
+                r = re.search(pattern, line).groups()
+            except AttributeError:
+                continue
+            rdate = datetime.datetime.strptime(r[0], "%Y-%m-%d %H:%M:%S")
+            logs.append((rdate, int(r[1]), r[2]))
+
+    return logs
 
 def concat(path, ext_list=["txt"]):
     """
-    Concatena il contenuto di tutti i file presenti nel path
+    Concatena il contenuto di tutti i file presenti nel path che hanno una
+    delle estensioni specificate
 
-    Ritorna una lista di stringhe, ogni stringa è una linea di log
-    """
-    log_files   = []
-    txt         = []
-    # Ciclo per concatenare il contenuto dei file di log
-    # Qui si va a supporre che nel path siano presenti *solo* i file che ci
-    # interessano. listdir non lista le directory speciali "." e ".." quindi
-    # non dovrebbero esserci problemi per un eventuale walk indesiderato
-    # attraverso il filesystem
-    for root, dirs, files in os.walk(path):
-        for name in files:
-            if (os.path.splitext(name)[1][1:] in ext_list):
-                log_files.append(os.path.join(root, name))
-
-    for f in log_files:
-        with open(f, "r") as inf:
-            txt.append(inf.read())
-    return "".join(txt).split("\n")[:-1]
-
-def makeTupList(txt, formatStr):
-    """
-    Crea una lista di tuple di tipo (datetime, lvl, str) a partire da una lista
-    di stringhe di testo formattate secondo "dataora,livello messaggio" e una
-    stringa di formato per parsare data e ora
-
-    Ritorna la lista di tuple
+    Ritorna una lista di tuple
     """
     logs = []
-    # Ogni tupla ha tre elementi: la data del messaggio di log in prima
-    # posizione, il livello di importanza del mesasggio in seconda posizione e
-    # il messaggio stesso in terza posizione.
-    # La list comprehension conterrà in output solo le stringe di testo che
-    # cominciano la sottostringa "Error", in modo tale da isolare gli errori
-    for line in txt:
-        tmp             = line.split(",", 1)
-        date_str        = tmp[0]
-        msg_lvl, msg    = tmp[1].split(" ", 1)
-        if msg.startswith("Error"):
-            logs.append(tuple([date_str, msg_lvl, msg]))
-    # Siccome le tuple sono immutabili si genera una nuova lista di tuple,
-    # questa volta aventi come primo elemento un oggetto datetime che viene
-    # creato parsando la data e l'ora secondo il formato dei file di log;
-    # il secondo elemento delle tuple viene lasciato invariato
-    logs =  [
-            (datetime.datetime.strptime(log[0], formatStr), int(log[1]), log[2])
-            for log in logs
-            ]
+    log_files = list_files(path, ext_list)
+    for f in log_files:
+        ff = filter_file(f)
+        logs.extend(ff) if ff else None
+
     return logs
 
-def sortLogs(logs):
+def sortl(logs):
     # La funzione sort viene automaticamente applicata al primo elemento di una
     # lista di tuple, quindi si ordina per data. Si vogliono i risultati a
     # partire dal più recente, quindi si inverte l'ordine di ordinamento
     return sorted(logs, reverse=True)
 
+def printl(logs):
+    """
+    Stampa a schermo il risultato in maniera leggibile
+    """
+    for l in logs:
+        print l[0], l[1], l[2]
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="path to log directory")
     parser.add_argument("-e", "--extensions", default="",
-                        help="list of valid estensions for log files")
-    args    = parser.parse_args()
-    path    = args.path
-    txt     = args.extensions                           \
-            and concat(path, args.extensions.split())   \
-            or concat(path)
-    # Le singole linee di testo sono ottenute splittando il contenuto dei due
-    # file ai caratteri di newline; siccome i file stessi terminano con un
-    # carattere di newline l'ultimo elemento anziché essere una linea di testo
-    # sarà una stringa vuota, quindi evito di considerarla con lo splice [:-1]
-    logs = makeTupList(txt, "%Y-%m-%d %H:%M:%S")
-    logs = sortLogs(logs)
-    # Infine si stampa il risultato
-    for l in logs:
-        print l[0], l[1], l[2]
+                        help="comma separated list of valid estensions for log files")
+    args = parser.parse_args()
+    path = args.path
+    logs = concat(path, args.extensions.split(",")) if args.extensions else concat(path)
+
+    logs = sortl(logs)
+    printl(logs)
--- es2/test_es2.py
+++ es2/test_es2.py
@@ -4,97 +4,103 @@ import os
 import unittest
 
 class TestLogParser(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestLogParser, self).__init__(*args, **kwargs)
 
-        self.concat_expected_result = \
+    log_files = []
+    filter_file_0_expected_res = []
+    concat_expected_res = []
+    sortl_expected_res = []
+    path = ""
+    valid_exts = []
+
+    @classmethod
+    def setUpClass(cls):
+
+        cls.log_files = \
         [
-        "2010-05-07 12:33:44,8 Error: a.txt"        ,
-        "2010-05-07 12:33:45,8 Error: a.txt"        ,
-        "2010-08-05 15:21:52,0 Not an error"        ,
-        "2013-06-27 18:12:11,8 Error: a.txt"        ,
-        "2008-10-11 19:21:22,8 Error: a.log"        ,
-        "2009-01-10 11:00:01,8 Error: a.log"        ,
-        "2010-08-05 15:21:52,0 Not an error"        ,
-        "2012-08-15 13:12:11,8 Error: a.log"        ,
-        "2015-05-07 12:33:44,8 Error: nested/a.txt" ,
-        "2010-08-05 15:21:52,0 Not an error"        ,
-        "2003-04-21 11:01:47,8 Error: nested/a.log" ,
-        "2010-08-05 15:21:52,0 Not an error"        ,
-        "2009-05-12 11:53:29,8 Error: nested/a.log"
+        "logs/testlogs/a.txt",
+        "logs/testlogs/a.log",
+        "logs/testlogs/nested/a.txt",
+        "logs/testlogs/nested/a.log"
         ]
 
-        self.makeTupList_expected_result = \
+        cls.filter_file_0_expected_res = \
         [
-        (datetime.datetime(2010, 5, 7, 12, 33, 44), 8, 'Error: a.txt')        ,
-        (datetime.datetime(2010, 5, 7, 12, 33, 45), 8, 'Error: a.txt')        ,
-        (datetime.datetime(2013, 6, 27, 18, 12, 11), 8, 'Error: a.txt')       ,
-        (datetime.datetime(2008, 10, 11, 19, 21, 22), 8, 'Error: a.log')      ,
-        (datetime.datetime(2009, 1, 10, 11, 0, 1), 8, 'Error: a.log')         ,
-        (datetime.datetime(2012, 8, 15, 13, 12, 11), 8, 'Error: a.log')       ,
-        (datetime.datetime(2015, 5, 7, 12, 33, 44), 8, 'Error: nested/a.txt') ,
-        (datetime.datetime(2003, 4, 21, 11, 1, 47), 8, 'Error: nested/a.log') ,
+        (datetime.datetime(2010, 5, 7, 12, 33, 44), 8, 'Error: a.txt'),
+        (datetime.datetime(2010, 5, 7, 12, 33, 45), 8, 'Error: a.txt'),
+        (datetime.datetime(2013, 6, 27, 18, 12, 11), 8, 'Error: a.txt')
+        ]
+
+        cls.concat_expected_res = \
+        [
+        (datetime.datetime(2010, 5, 7, 12, 33, 44), 8, 'Error: a.txt'),
+        (datetime.datetime(2010, 5, 7, 12, 33, 45), 8, 'Error: a.txt'),
+        (datetime.datetime(2013, 6, 27, 18, 12, 11), 8, 'Error: a.txt'),
+        (datetime.datetime(2008, 10, 11, 19, 21, 22), 8, 'Error: a.log'),
+        (datetime.datetime(2009, 1, 10, 11, 0, 1), 8, 'Error: a.log'),
+        (datetime.datetime(2012, 8, 15, 13, 12, 11), 8, 'Error: a.log'),
+        (datetime.datetime(2015, 5, 7, 12, 33, 44), 8, 'Error: nested/a.txt'),
+        (datetime.datetime(2003, 4, 21, 11, 1, 47), 8, 'Error: nested/a.log'),
         (datetime.datetime(2009, 5, 12, 11, 53, 29), 8, 'Error: nested/a.log')
         ]
 
-        self.sortLogs_expected_result = \
+        cls.sortl_expected_res = \
         [
-        (datetime.datetime(2015, 5, 7, 12, 33, 44), 8, 'Error: nested/a.txt') ,
-        (datetime.datetime(2013, 6, 27, 18, 12, 11), 8, 'Error: a.txt')       ,
-        (datetime.datetime(2012, 8, 15, 13, 12, 11), 8, 'Error: a.log')       ,
-        (datetime.datetime(2010, 5, 7, 12, 33, 45), 8, 'Error: a.txt')        ,
-        (datetime.datetime(2010, 5, 7, 12, 33, 44), 8, 'Error: a.txt')        ,
+        (datetime.datetime(2015, 5, 7, 12, 33, 44), 8, 'Error: nested/a.txt'),
+        (datetime.datetime(2013, 6, 27, 18, 12, 11), 8, 'Error: a.txt'),
+        (datetime.datetime(2012, 8, 15, 13, 12, 11), 8, 'Error: a.log'),
+        (datetime.datetime(2010, 5, 7, 12, 33, 45), 8, 'Error: a.txt'),
+        (datetime.datetime(2010, 5, 7, 12, 33, 44), 8, 'Error: a.txt'),
         (datetime.datetime(2009, 5, 12, 11, 53, 29), 8, 'Error: nested/a.log'),
-        (datetime.datetime(2009, 1, 10, 11, 0, 1), 8, 'Error: a.log')         ,
-        (datetime.datetime(2008, 10, 11, 19, 21, 22), 8, 'Error: a.log')      ,
+        (datetime.datetime(2009, 1, 10, 11, 0, 1), 8, 'Error: a.log'),
+        (datetime.datetime(2008, 10, 11, 19, 21, 22), 8, 'Error: a.log'),
         (datetime.datetime(2003, 4, 21, 11, 1, 47), 8, 'Error: nested/a.log')
         ]
 
-        self.path       = "./logs/testlogs/"
-        self.valid_exts = ["txt", "log"]
+        cls.path       = "logs/testlogs/"
+        cls.valid_exts = ["txt", "log"]
 
-    def testConcatLength(self):
+    def test_list_files(self):
         """
-        Testa che la lettura e lo split dei file sia corretto
+        Verifica che tutti e solo i file giusti vengano selezionati per essere
+        letti
+        """
+        self.assertEqual(es2.list_files(self.path, self.valid_exts),
+                         self.log_files)
+
+    def test_filter_file(self):
+        """
+        Verifica che i file vengano filtrati correttamente
+        """
+        self.assertEqual(es2.filter_file(self.log_files[0]),
+                         self.filter_file_0_expected_res)
+
+    def test_concat_length(self):
+        """
+        Verifica che la lettura e lo split dei file sia corretto
         """
         self.assertEqual(len(es2.concat(self.path, self.valid_exts)),
-                len(self.concat_expected_result))
+                         len(self.concat_expected_res))
 
-    def testConcatResult(self):
+    def test_concat_res(self):
         """
-        Testa che il risultato di concat sia corretto
+        Verifica che il risultato di concat sia corretto
         """
         self.assertEqual(es2.concat(self.path, self.valid_exts),
-                self.concat_expected_result)
+                         self.concat_expected_res)
 
-    def testMakeTupListLength(self):
+    def test_sortl(self):
         """
-        Testa che la lunghezza della lista di tuple restituita da makeTupList
-        sia corretta
+        Verifica che i log vengano ordinati nel modo giusto
         """
-        self.assertEqual(len(self.makeTupList_expected_result),
-                len(es2.makeTupList(self.concat_expected_result, "%Y-%m-%d %H:%M:%S")))
+        self.assertEqual(es2.sortl(self.concat_expected_res),
+                         self.sortl_expected_res)
 
-    def testMakeTupListResult(self):
+    def test_concat_sanity_check(self):
         """
-        Testa che il risultato di makeTupList sia corretto
-        """
-        self.assertEqual(self.makeTupList_expected_result,
-                es2.makeTupList(self.concat_expected_result, "%Y-%m-%d %H:%M:%S"))
-
-    def testSortLogs(self):
-        """
-        Controlla che i log vengano ordinati nel modo giusto
-        """
-        self.assertEqual(es2.sortLogs(self.makeTupList_expected_result),
-                self.sortLogs_expected_result)
-
-    def testMakeTupListSanityCheck(self):
-        """
-        Controlla che ogni tupla generata da makeTupList contenga davvero
+        Verifica che ogni tupla generata da concat contenga davvero
         3 elementi di tipo (datetime, int, str)
         """
-        tl = es2.makeTupList(self.concat_expected_result, "%Y-%m-%d %H:%M:%S")
+        tl = es2.concat(self.path, self.valid_exts)
         for t in tl:
             self.assertTrue(len(t) == 3)
             self.assertTrue(type(t[0]) == datetime.datetime)
