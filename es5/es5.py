3f583f2c03d3d85a8ffcba7ae5d94af95439d809
Luca Benci <zero@develer.com>
2015-11-12 09:59:35 +0100

    Superati test

--- es5/es5.py
+++ es5/es5.py
@@ -1,4 +1,23 @@
 #! /usr/bin/python
 #-*- coding:utf-8 -*-
 
+import argparse
+import urlparse
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="The url of the webpage to download")
+    parser.parse_args()
+
+
+def validate(url):
+    valid_schemes = ["http", "https"]
+    parse_res = urlparse.urlparse(url)
+    if parse_res.scheme in valid_schemes:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    main()
