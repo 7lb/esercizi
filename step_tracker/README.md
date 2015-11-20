5c9487a3b59b818751a31b4333bb5fc9d3b51366
Luca Benci <zero@develer.com>
2015-11-20 15:33:50 +0100

    Aggiunta gestione di più utenti e aggiornato readme

--- step_tracker/README.md
+++ step_tracker/README.md
@@ -1,6 +1,7 @@
 # Create
 ### Aggiunta passi per la data corrente
 POST /v1/days
+POST /v2/users/[user]/days
 ##### Parametri:
 * steps: numero di passi effettuati
 
@@ -21,6 +22,7 @@ POST /v1/days
 # Retrieve
 ### Recupero passi per una data arbitraria
 GET /v1/days/[data ISO 8601]
+GET /v2/users/[user]/days/[data ISO 8601]
 
 ##### Ritorna:
 200 OK:
@@ -37,6 +39,7 @@ GET /v1/days/[data ISO 8601]
 # Update
 ### Modifica passi per la data corrente
 PUT /v1/days
+PUT /v2/users/[user]/days
 ##### Parametri:
 * steps: numero di passi da aggiornare
 
@@ -56,6 +59,7 @@ PUT /v1/days
 # Delete
 ### Eliminazione passi per una data arbitraria
 DELETE /v1/days/[data ISO 8601]
+DELETE /v2/users/[user]/days/[data ISO 8601]
 
 ##### Ritorna:
 200 OK
--- step_tracker/tracker.py
+++ step_tracker/tracker.py
@@ -10,10 +10,13 @@ app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 # Backend
-days = {}
+collection = {}
 
 users = {
     "admin": "admin",
+    "pippo": "pluto",
+    "foo": "bar",
+    "paolo": "i<3francesca",
 }
 
 @auth.get_password
@@ -33,10 +36,29 @@ def unauthorized():
     )
 
 
+@app.errorhandler(405)
+def method_not_allowed():
+    return make_response(
+        jsonify({"message": "Method not allowed"}), 405)
+
+
+@app.errorhandler(500)
+def internal_server_error():
+    return make_response(
+        jsonify({"message": "Internal server error"}), 500)
+
+
+def check_user(user):
+    if user != auth.username() and user is not None:
+        return unauthorized()
+
 # Aggiunta di un numero di passi per la giornata in corso
 @app.route("/v1/days", methods=["POST"])
+@app.route("/v2/users/<user>/days", methods=["POST"])
 @auth.login_required
-def add_day():
+def add_day(user=None):
+    check_user(user)
+
     if not request.json or not "steps" in request.json:
         return make_response(
             jsonify({"message": "Bad Request: missing step number"}), 400
@@ -48,25 +70,32 @@ def add_day():
         "steps": request.json["steps"]
     }
 
-    if day_present(day, days):
+    if day_present(day, collection):
         return make_response(
             jsonify({"message": "Bad Request: item already present"}), 400
         )
 
-    add(day, days)
-    return make_response(
-        jsonify({
-            "message": "Created",
-            "day": day,
-            "uri": url_for("get_day", date=date, _external=True)
-        }), 201
-    )
+    add(day, collection)
+    response = {
+        "message": "Created",
+        "day": day,
+    }
+
+    if user is not None:
+        response["uri"] = url_for(
+            "get_day", date=date, user=user, _external=True)
+    else:
+        response["uri"] = url_for("get_day", date=date, _external=True)
+    return make_response(jsonify(response), 201)
 
 
 # Modifica del numero di passi per la giornata in corso
 @app.route("/v1/days", methods=["PUT"])
+@app.route("/v2/users/<user>/days", methods=["PUT"])
 @auth.login_required
-def change_day():
+def change_day(user=None):
+    check_user(user)
+
     if not request.json or not "steps" in request.json:
         make_response(
             jsonify({"message": "Bad Request: missing step number"}), 400
@@ -78,29 +107,36 @@ def change_day():
         "steps": request.json["steps"]
     }
 
-    if not day_present(day, days):
+    if not day_present(day, collection):
         return make_response(
             jsonify({"message": "Not Found"}), 404
         )
 
     # Siccome remove e add usano la data come chiave il seguente codice
     # funziona perché i corpi del giorno rimosso e aggiunto differiscono
-    remove(day, days)
-    add(day, days)
-    return make_response(
-        jsonify({
-            "message": "OK",
-            "day": day,
-            "uri": url_for("get_day", date=date, _external=True)
-        }), 200
-    )
+    remove(day, collection)
+    add(day, collection)
+    response = {
+        "message": "OK",
+        "day": day,
+    }
+
+    if user is not None:
+        response["uri"] = url_for(
+                "get_day", date=date, user=user, _external=True)
+    else:
+        response["uri"] = url_for("get_day", date=date, _external=True)
+    return make_response(jsonify(response), 200)
 
 
 # Recupero del numero di passi per una data arbitraria
 @app.route("/v1/days/<date>", methods=["GET"])
+@app.route("/v2/users/<user>/days/<date>", methods=["GET"])
 @auth.login_required
-def get_day(date):
-    day = days.get(date)
+def get_day(date, user=None):
+    check_user(user)
+
+    day = get_date(date, collection)
     if not day:
         return make_response(
             jsonify({
@@ -118,9 +154,12 @@ def get_day(date):
 
 # Rimozione dei passi registrati per una data arbitraria
 @app.route("/v1/days/<date>", methods=["DELETE"])
+@app.route("/v2/users/<user>/days/<date>", methods=["DELETE"])
 @auth.login_required
-def remove_day(date):
-    day = days.get(date)
+def remove_day(date, user=None):
+    check_user(user)
+
+    day = get_date(date, collection)
     if not day:
         return make_response(
             jsonify({
@@ -128,7 +167,7 @@ def remove_day(date):
             }), 404
         )
 
-    remove(day, days)
+    remove(day, collection)
     return make_response(
         jsonify({
             "message": "OK",
@@ -136,25 +175,55 @@ def remove_day(date):
         }), 200
     )
 
-def day_present(day, days):
+
+def day_present(day, collection):
     """
     Controlla se il giorno è presente nella collection
     """
+    days = collection.get(auth.username())
+
+    if days is None:
+        return None
+
     return days.get(day["date"])
 
 
-def add(day, days):
+def add(day, collection):
     """
     Aggiunge un giorno alla collection
     """
+    days = collection.get(auth.username())
+
+    if days is None:
+        days = {}
+
     days[day["date"]] = day
+    collection[auth.username()] = days
 
 
-def remove(day, days):
+def remove(day, collection):
     """
     Rimuove un giorno dalla collection
     """
+    days = collection.get(auth.username())
+
+    if days is None:
+        return None
+
     return days.pop(day["date"], None)
+
+
+def get_date(date, collection):
+    """
+    Ritorna un giorno dalla collection
+    """
+    days = collection.get(auth.username())
+
+    if days is None:
+        return None
+
+    return days.get(date)
+
 
 if __name__ == "__main__":
     app.run()
